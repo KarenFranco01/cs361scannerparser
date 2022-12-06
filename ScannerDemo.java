8a99ab0b225f632ca5def6ba29d297fbe2ae3ace
KarenFranco <kf07575n@pace.edu>
2022-12-05 23:19:11 -0500

    ScannerDemo

--- ScannerDemo.java
+++ ScannerDemo.java
@@ -1,25 +1,22 @@
-
 /**
  * @author scharffc
  * 
  */
-public class ScannerDemo {
+public class ScannerDemo{
 
-	// Oh! This needs to be changed !
-	// this is her file private static String file1 = "C:\\Users\\cscharff\\eclipse-workspace\\ScannerSolution\\src\\prog1.kay";
-	private static String file1 = "C:\Users\karen\source\repos\cs361scannerparser\\prog1.kay";
-	private static int counter = 1;
+    // Oh! This needs to be changed !
+    private static String file1 = "C:\Users\karen\source\repos\cs361scannerparser\\prog1.kay";
+    private static int counter = 1;
 
-	public static void main(String args[]) {
-		TokenStream ts = new TokenStream(file1);
-		System.out.println(file1);
-		Token tk;
-		while (!ts.isEndofFile()) {
-			// TODO TO BE COMPLETED
-			// tk is not defined on the line below
-			tk = ts.nextToken(); 
-			System.out.println("Token " + counter++ + " - Type: " + tk.getType() + " - Value: " + tk.getValue());
-		}
-	}
+    public static void main(String args[]) {
+        TokenStream ts = new TokenStream(file1);
+        Token tk;
+        System.out.println(file1);
+        while (!ts.isEndofFile()) {
+            tk = ts.nextToken();
+            // TODO TO BE COMPLETED
+            // tk is not defined on the line below
+             System.out.println("Token " + counter++ + " - Type: " + tk.getType() + " - Value: " + tk.getValue());
+        }
+    }
 }
-
